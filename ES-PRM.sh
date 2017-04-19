fe7667f2ac065fb4b3b4f1c05ced73c4f72ce9cc
marwanes90 <marwanes90@gmail.com>
2017-04-19 10:57:19 +0200

    updated on 19/04/2017 10:57:19

--- ES-PRM.sh
+++
@@ -1,4 +0,0 @@
-curl 'https://d.la3-c2-chi.salesforceliveagent.com/chat/rest/Visitor/Availability?Availability.prefix=Visitor&Availability.ids=\[57312000000TNfE,57312000000TNfi,57312000000TNfd,57312000000TNfY,57312000000TNfT,57312000000TNfO,57312000000TNfJ\]&deployment_id=57212000000GpJp&org_id=00DA0000000abSm' -H "X-LIVEAGENT-API-VERSION: 37" -H "Content-Type: application/json" | python -mjson.tool > report.json
-git add report.json
-dt=$(date '+%d/%m/%Y %H:%M:%S');
-git commit -m "updated on $dt"
